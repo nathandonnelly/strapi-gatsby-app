f186a782d95c6cf2893ab4e4c29042ad3266b8b3
Nathan Donnelly <email@nathandonnelly.com>
2025-11-24 21:56:05 -0500

    PostCSS/TailwindCss fix

--- frontend/gatsby-config.ts
+++ frontend/gatsby-config.ts
@@ -1,10 +1,11 @@
 import type { GatsbyConfig } from "gatsby";
 //gatsby-config.js
+
 require("dotenv").config({
   path: `.env.${process.env.NODE_ENV}`,
 });
 
-const strapiConfig = {
+const StrapiConfig = {
   apiURL: process.env.STRAPI_API_URL || "http://127.0.0.1:1337",
   collectionTypes: ["post"],
 };
@@ -14,11 +15,14 @@ const config: GatsbyConfig = {
     title: `frontend`,
     siteUrl: `https://www.yourdomain.tld`,
   },
+  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
+  // If you use VSCode you can also use the GraphQL plugin
+  // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
   plugins: [
     {
       resolve: `gatsby-source-strapi`,
-      options: strapiConfig,
+      options: StrapiConfig,
     },
     "gatsby-transformer-remark",
     "gatsby-plugin-postcss",
