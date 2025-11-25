bfc8f57221431ac62be6192566e33c61d0cd2539
Nathan Donnelly <email@nathandonnelly.com>
2025-11-24 21:22:41 -0500

    Gatsby config update for dev environment setup.

--- frontend/gatsby-config.ts
+++ frontend/gatsby-config.ts
@@ -1,15 +1,28 @@
 import type { GatsbyConfig } from "gatsby";
+//gatsby-config.js
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
+const strapiConfig = {
+  apiURL: process.env.STRAPI_API_URL || "http://127.0.0.1:1337",
+  collectionTypes: ["post"],
+};
 
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `frontend`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
   },
-  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
-  // If you use VSCode you can also use the GraphQL plugin
-  // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-postcss"]
+  plugins: [
+    {
+      resolve: `gatsby-source-strapi`,
+      options: strapiConfig,
+    },
+    "gatsby-transformer-remark",
+    "gatsby-plugin-postcss",
+  ],
 };
 
 export default config;
